7458a6b953f0ba049d0ee8ea6a050453abce9513
grande <87326435+ElderliDrvr@users.noreply.github.com>
2024-02-12 00:18:52 +0900

    Update linetrace_sensor.py

--- linetrace_sensor.py
+++ linetrace_sensor.py
@@ -4,37 +4,38 @@ import time
 # GPIOピンの設定
 GPIO.setmode(GPIO.BOARD)
 
-SENSOR1 = 19
-SENSOR2 = 26
+SENSOR1 = 29
+SENSOR2 = 31
 GPIO.setup(SENSOR1, GPIO.IN)  # IRセンサー1の入力ピン
 GPIO.setup(SENSOR2, GPIO.IN)  # IRセンサー2の入力ピン
 
+# ubuntu de nihongo douyatte utsunenn
 def linetrace():
     while True:
-        if GPIO.input(SENSOR1) == GPIO.LOW and GPIO.input(SENSOR2) == GPIO.LOW:
+        if GPIO.input(SENSOR1) == GPIO.HIGH and GPIO.input(SENSOR2) == GPIO.HIGH:
             # 直進
-            print("両方のセンサーでラインが検出されました")
+            print("heres MID!")
             # ロボットの動作を制御するためのコードを追加する
 
-        elif GPIO.input(SENSOR1) == GPIO.LOW:
+        elif GPIO.input(SENSOR1) == GPIO.HIGH:
             # 左右のどちらかに旋回
-            print("センサー1でラインが検出されました")
+            print("heres LEFT, Go Right!")
             # ロボットの動作を制御するためのコードを追加する
 
-        elif GPIO.input(SENSOR2) == GPIO.LOW:
+        elif GPIO.input(SENSOR2) == GPIO.HIGH:
             # 左右のどちらかに旋回
-            print("センサー2でラインが検出されました")
+            print("heres RIGHT, Go Left!")
             # ロボットの動作を制御するためのコードを追加する
 
         else:
             # 停止 手動操縦でラインまで復帰できるようにするのもアリ
-            print("ラインが検出されませんでした")
+            print("where am i?")
             # ロボットの動作を制御するためのコードを追加する
 
-        time.sleep(0.1)  # 安定のための遅延
+        time.sleep(1)  # 安定のための遅延
 
 # 実行
-# linetrace()
+linetrace()
 
 # GPIOピンのクリーンアップ
 GPIO.cleanup()
