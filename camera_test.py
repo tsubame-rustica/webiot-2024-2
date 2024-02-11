2f2af914aec4e3919b8437c15808b2f3b05e30d8
webiot2024 <webiot2024@None.com>
2024-02-11 14:13:51 +0900

    cameratest

---
+++ camera_test.py
@@ -0,0 +1,33 @@
+# source: https://note.nkmk.me/python-pyzbar-barcode-qrcode/
+import cv2
+from pyzbar.pyzbar import decode
+
+camera_id = 0
+delay = 1
+window_name = 'OpenCV pyzbar'
+
+cap = cv2.VideoCapture(camera_id)
+
+# バーコードの値を取得するだけならimshowなどは不要
+def detect_barcode(delay, window_name, cap):
+    while True:
+        ret, frame = cap.read()
+
+        if ret:
+            for d in decode(frame):
+                s = d.data.decode()
+                #print(s)
+                frame = cv2.rectangle(frame, (d.rect.left, d.rect.top), (d.rect.left + d.rect.width, d.rect.top + d.rect.height), (0, 255, 0), 3)
+                frame = cv2.putText(frame, s, (d.rect.left, d.rect.top + d.rect.height), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2, cv2.LINE_AA)
+            cv2.imshow(window_name, frame)
+
+        if cv2.waitKey(delay) & 0xFF == ord('q'):
+            break
+
+    # return s
+
+detect_barcode(delay, window_name, cap)
+
+# clear
+cap.release()
+cv2.destroyWindow(window_name)
